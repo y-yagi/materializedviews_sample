2e7d5871ab40b08f064cd5a79739070b2dc7bb0c
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2015-05-28 07:44:27 +0900

    update sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 unless Author.exists?
-  (1..20).each do |i|
+  200.times.each do |i|
     Author.create!(name: "author_#{i}")
   end
 end
 
 unless Book.exists?
-  (1..100).each do |i|
+  1000.times.each do |i|
     Book.create!(name: "book_#{i}", price: rand(100) * 100, author: Author.find((1..20).to_a.sample))
   end
 end
 
 unless Sale.exists?
-  (1..1000).each do |_|
+  10000.times.each do |_|
     Sale.create!(
       number: rand(2) + 1, date: (1..7).to_a.sample.days.ago,
       book: Book.find((1..100).to_a.sample)
